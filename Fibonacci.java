b3b6662d3f538da23093f54b9dcbb61b93768e61
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-10-25 17:04:24 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+public class Fibonacci
+{
+    public static int fibonacciRecursion(int n)
+    {
+        if(n == 0)
+        {
+            return 0;
+        }
+        if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        return fibonacciRecursion(n-2) + fibonacciRecursion(n-1);
+    }
+    public static void main(String args[])
+    {
+        int maxNumber = 9;
+        System.out.println("Fibonacci Series of "+maxNumber+ " numbers: ");
+        for (int i = 0;i<maxNumber;i++)
+        {
+            System.out.println(fibonacciRecursion(i) + " ");
+        }
+    }
+}
